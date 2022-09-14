242ee0c2a369e02c5fa209b335cb3be4e765a713
Maclain Wiltzer <mwiltzer@gmail.com>
2022-09-14 09:01:23 -0400

    Init repo with source from makolyte

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Configuration;
+
+namespace WeatherClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = (WeatherClientConfig)ConfigurationManager.GetSection(nameof(WeatherClientConfig));
+
+            Console.WriteLine(config.WeatherAPIUrl);
+            Console.WriteLine(config.IsEnabled);
+            Console.WriteLine(config.Timeout);
+        }
+    }
+}
---
+++ WeatherClientConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace WeatherClient
+{
+    public class WeatherClientConfig
+    {
+        public bool IsEnabled { get; set; } = false;
+        public string WeatherAPIUrl { get; set; }
+        public string Timeout { get; set; }
+    }
+}
